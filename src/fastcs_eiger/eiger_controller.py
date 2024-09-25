13339d5625a3770972872a03f43ef229fea0b7d8
James Souter <james.souter@diamond.ac.uk>
2024-09-25 10:25:43 +0100

    Separate subsystems into separate controllers

--- src/fastcs_eiger/eiger_controller.py
+++ src/fastcs_eiger/eiger_controller.py
@@ -7,7 +7,7 @@ from typing import Any, Literal
 
 import numpy as np
 from fastcs.attributes import Attribute, AttrR, AttrRW, AttrW
-from fastcs.controller import Controller
+from fastcs.controller import Controller, SubController
 from fastcs.datatypes import Bool, Float, Int, String
 from fastcs.wrappers import command, scan
 from PIL import Image
@@ -140,13 +140,6 @@ class EigerParameter:
     """Mode of parameter within subsystem."""
     response: dict[str, Any]
     """JSON response from GET of parameter."""
-    has_unique_key: bool = True
-    """Whether this parameter has a unique key across all subsystems."""
-
-    @property
-    def name(self) -> str:
-        """Unique name of parameter across all subsystems."""
-        return self.key if self.has_unique_key else f"{self.subsystem}_{self.key}"
 
     @property
     def uri(self) -> str:
@@ -156,6 +149,10 @@ class EigerParameter:
 
 EIGER_PARAMETER_SUBSYSTEMS = EigerParameter.__annotations__["subsystem"].__args__
 EIGER_PARAMETER_MODES = EigerParameter.__annotations__["mode"].__args__
+
+
+def _key_to_attribute_name(key: str):
+    return key.replace("/", "_")
 
 
 class EigerController(Controller):
@@ -180,7 +177,6 @@ class EigerController(Controller):
         self.connection = HTTPConnection(self._ip, self._port)
 
         # Parameter update logic
-        self._parameter_updates: set[str] = set()
         self._parameter_update_lock = asyncio.Lock()
 
     async def initialise(self) -> None:
@@ -198,105 +194,14 @@ class EigerController(Controller):
             await self.initialize()
 
         try:
-            parameters = await self._introspect_detector()
+            for subsystem in EIGER_PARAMETER_SUBSYSTEMS:
+                controller = EigerSubsystemController(
+                    subsystem, self.connection, self._parameter_update_lock)
+                self.register_sub_controller(subsystem, controller)
+                await controller.initialise()
         except HTTPRequestError:
             print("\nAn HTTP request failed while introspecting detector:\n")
             raise
-
-        attributes = self._create_attributes(parameters)
-
-        for name, attribute in attributes.items():
-            setattr(self, name, attribute)
-
-    async def _introspect_detector(self) -> list[EigerParameter]:
-        parameters = []
-        for subsystem, mode in product(
-            EIGER_PARAMETER_SUBSYSTEMS, EIGER_PARAMETER_MODES
-        ):
-            subsystem_keys = [
-                parameter
-                for parameter in await self.connection.get(
-                    f"{subsystem}/api/1.8.0/{mode}/keys"
-                )
-                if parameter not in IGNORED_KEYS
-            ] + MISSING_KEYS[subsystem][mode]
-            requests = [
-                self.connection.get(f"{subsystem}/api/1.8.0/{mode}/{key}")
-                for key in subsystem_keys
-            ]
-            responses = await asyncio.gather(*requests)
-
-            parameters.extend(
-                [
-                    EigerParameter(
-                        key=key, subsystem=subsystem, mode=mode, response=response
-                    )
-                    for key, response in zip(subsystem_keys, responses, strict=False)
-                ]
-            )
-
-        return parameters
-
-    def _create_attributes(self, parameters: list[EigerParameter]):
-        """Create ``Attribute``s from ``EigerParameter``s.
-
-        Args:
-            parameters: ``EigerParameter``s to create ``Attributes`` from
-
-        """
-        self._tag_key_clashes(parameters)
-
-        attributes: dict[str, Attribute] = {}
-        for parameter in parameters:
-            group = f"{parameter.subsystem.capitalize()}{parameter.mode.capitalize()}"
-            match parameter.response["value_type"]:
-                case "float":
-                    datatype = Float()
-                case "int" | "uint":
-                    datatype = Int()
-                case "bool":
-                    datatype = Bool()
-                case "string" | "datetime" | "State" | "string[]":
-                    datatype = String()
-                case _:
-                    print(f"Failed to handle {parameter}")
-
-            # Flatten nested uri keys - e.g. threshold/1/mode -> threshold_1_mode
-            attribute_name = parameter.name.replace("/", "_")
-
-            match parameter.response["access_mode"]:
-                case "r":
-                    attributes[attribute_name] = AttrR(
-                        datatype,
-                        handler=EIGER_HANDLERS[parameter.mode](parameter.uri),
-                        group=group,
-                    )
-                case "rw":
-                    attributes[attribute_name] = AttrRW(
-                        datatype,
-                        handler=EIGER_HANDLERS[parameter.mode](parameter.uri),
-                        group=group,
-                        allowed_values=parameter.response.get("allowed_values", None),
-                    )
-
-        return attributes
-
-    @staticmethod
-    def _tag_key_clashes(parameters: list[EigerParameter]):
-        """Find key clashes between subsystems and tag parameters to use extended name.
-
-        Modifies list of parameters in place.
-
-        Args:
-            parameters: Parameters to search
-
-        """
-        for idx, parameter in enumerate(parameters):
-            for other in parameters[idx + 1 :]:
-                if parameter.key == other.key:
-                    parameter.has_unique_key = False
-                    other.has_unique_key = False
-                    break
 
     @detector_command
     async def initialize(self):
@@ -328,46 +233,13 @@ class EigerController(Controller):
     async def cancel(self):
         await self.connection.put(command_uri("cancel"))
 
-    async def queue_update(self, parameters: list[str]):
-        """Add the given parameters to the list of parameters to update.
-
-        Args:
-            parameters: Parameters to be updated
-
-        """
-        async with self._parameter_update_lock:
-            for parameter in parameters:
-                self._parameter_updates.add(parameter)
-
-            await self.stale_parameters.set(True)
-
     @scan(0.1)
     async def update(self):
         """Periodically check for parameters that need updating from the detector."""
-        if not self._parameter_updates:
-            if self.stale_parameters.get():
-                await self.stale_parameters.set(False)
-
-            return
-
-        # Take a copy of the current parameters and clear. Parameters may be repopulated
-        # during this call and need to be updated again immediately.
-        async with self._parameter_update_lock:
-            parameters = self._parameter_updates.copy()
-            self._parameter_updates.clear()
-
-        # Release lock while fetching parameters - this may be slow
-        parameter_updates: list[Coroutine] = []
-        for parameter in parameters:
-            match getattr(self, parameter):
-                # TODO: mypy doesn't understand AttrR as a type for some reason:
-                # `error: Expected type in class pattern; found "Any"  [misc]`
-                case AttrR(updater=EigerConfigHandler() as updater) as attr:  # type: ignore [misc]
-                    parameter_updates.append(updater.config_update(self, attr))
-                case _:
-                    print(f"Failed to handle update for {parameter}")
-
-        await asyncio.gather(*parameter_updates)
+        await self.stale_parameters.set(
+            any(c.stale_parameters.get() for c in self.get_sub_controllers().values()))
+        controller_updates = [c.update() for c in self.get_sub_controllers().values()]
+        await asyncio.gather(*controller_updates)
 
     @scan(1)
     async def handle_monitor(self):
@@ -382,3 +254,128 @@ class EigerController(Controller):
 
             # TODO: Populate waveform PV to display as image, once supported in PVI
             print(np.array(image))
+
+
+class EigerSubsystemController(SubController):
+
+    stale_parameters = AttrR(Bool())
+
+    def __init__(self, subsystem: str, connection: HTTPConnection, lock: asyncio.Lock):
+        self._subsystem = subsystem
+        self.connection = connection
+        self._parameter_update_lock = lock
+        self._parameter_updates: set[str] = set()
+        super().__init__()
+
+    async def _introspect_detector_subsystem(self) -> list[EigerParameter]:
+        parameters = []
+        for mode in EIGER_PARAMETER_MODES:
+            subsystem_keys = [
+                parameter
+                for parameter in await self.connection.get(
+                    f"{self._subsystem}/api/1.8.0/{mode}/keys"
+                )
+                if parameter not in IGNORED_KEYS
+            ] + MISSING_KEYS[self._subsystem][mode]
+            requests = [
+                self.connection.get(f"{self._subsystem}/api/1.8.0/{mode}/{key}")
+                for key in subsystem_keys
+            ]
+            responses = await asyncio.gather(*requests)
+
+            parameters.extend(
+                [
+                    EigerParameter(
+                        key=key, subsystem=self._subsystem, mode=mode, response=response
+                    )
+                    for key, response in zip(subsystem_keys, responses, strict=False)
+                ]
+            )
+
+        return parameters
+
+    async def initialise(self) -> None:
+        parameters = await self._introspect_detector_subsystem()
+        attributes = self._create_attributes(parameters)
+
+        for name, attribute in attributes.items():
+            setattr(self, name, attribute)
+
+    def _create_attributes(self, parameters: list[EigerParameter]):
+        """Create ``Attribute``s from ``EigerParameter``s.
+
+        Args:
+            parameters: ``EigerParameter``s to create ``Attributes`` from
+
+        """
+        attributes: dict[str, Attribute] = {}
+        for parameter in parameters:
+            group = f"{parameter.subsystem.capitalize()}{parameter.mode.capitalize()}"
+            match parameter.response["value_type"]:
+                case "float":
+                    datatype = Float()
+                case "int" | "uint":
+                    datatype = Int()
+                case "bool":
+                    datatype = Bool()
+                case "string" | "datetime" | "State" | "string[]":
+                    datatype = String()
+                case _:
+                    print(f"Failed to handle {parameter}")
+
+            # Flatten nested uri keys - e.g. threshold/1/mode -> threshold_1_mode
+            attribute_name = _key_to_attribute_name(parameter.key)
+
+            match parameter.response["access_mode"]:
+                case "r":
+                    attributes[attribute_name] = AttrR(
+                        datatype,
+                        handler=EIGER_HANDLERS[parameter.mode](parameter.uri),
+                        group=group,
+                    )
+                case "rw":
+                    attributes[attribute_name] = AttrRW(
+                        datatype,
+                        handler=EIGER_HANDLERS[parameter.mode](parameter.uri),
+                        group=group,
+                        allowed_values=parameter.response.get("allowed_values", None),
+                    )
+
+        return attributes
+
+    async def queue_update(self, parameters: list[str]):
+        """Add the given parameters to the list of parameters to update.
+
+        Args:
+            parameters: Parameters to be updated
+
+        """
+        async with self._parameter_update_lock:
+            for parameter in parameters:
+                self._parameter_updates.add(parameter)
+
+            await self.stale_parameters.set(True)
+
+    async def update(self):
+        if not self._parameter_updates:
+            if self.stale_parameters.get():
+                await self.stale_parameters.set(False)
+
+        async with self._parameter_update_lock:
+            parameters = self._parameter_updates.copy()
+            self._parameter_updates.clear()
+
+        # Release lock while fetching parameters - this may be slow
+        parameter_updates: list[Coroutine] = []
+        for parameter in parameters:
+            if parameter in IGNORED_KEYS:
+                continue
+            attr_name = _key_to_attribute_name(parameter)
+            match getattr(self, attr_name, None):
+                # TODO: mypy doesn't understand AttrR as a type for some reason:
+                # `error: Expected type in class pattern; found "Any"  [misc]`
+                case AttrR(updater=EigerConfigHandler() as updater) as attr:  # type: ignore [misc]
+                    parameter_updates.append(updater.config_update(self, attr))
+                case _ as attr:
+                    print(f"Failed to handle update for {parameter}: {attr}")
+        await asyncio.gather(*parameter_updates)
