f3038e7859933cc9edc77710f1da1c4bd515b289
Paula <paulamartingmz@gmail.com>
2025-04-15 14:45:07 +0000

    Remove unreachable case

--- src/fastcs_eiger/eiger_parameter.py
+++ src/fastcs_eiger/eiger_parameter.py
@@ -24,8 +24,6 @@ class EigerParameterResponse(BaseModel):
                 return Bool()
             case "string" | "datetime" | "State" | "string[]":
                 return String()
-            case _:
-                raise ValueError(f"Unsupported data type: {self.value_type}")
 
 
 @dataclass
