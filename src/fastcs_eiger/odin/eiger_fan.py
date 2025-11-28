26227abcdf49af20b1f0cd2f60c623c92cfea62e
Gary Yendell <gary.yendell@diamond.ac.uk>
2025-11-28 14:09:24 +0000

    Add fan Ready PV

--- src/fastcs_eiger/odin/eiger_fan.py
+++ src/fastcs_eiger/odin/eiger_fan.py
@@ -1,9 +1,14 @@
+from fastcs.attributes import AttrR
+from fastcs.datatypes import Bool
+from fastcs_odin.io.status_summary_attribute_io import StatusSummaryAttributeIORef
 from fastcs_odin.odin_subcontroller import OdinSubController
 from fastcs_odin.util import create_attribute
 
 
 class EigerFanAdapterController(OdinSubController):
     """Controller for an EigerFan adapter in an odin control server"""
+
+    state: AttrR[str]
 
     async def initialise(self):
         for parameter in self.parameters:
@@ -15,3 +20,13 @@ class EigerFanAdapterController(OdinSubController):
                 parameter.name,
                 create_attribute(parameter=parameter, api_prefix=self._api_prefix),
             )
+
+        # Manually validate `state` to get a nicer error message if not introspected
+        self._validate_hinted_attributes()
+
+        self.ready = AttrR(
+            Bool(),
+            io_ref=StatusSummaryAttributeIORef(
+                [], "", lambda states: states[0] == "DSTR_HEADER", [self.state]
+            ),
+        )
