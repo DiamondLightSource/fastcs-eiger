9ba8180c65247b998914d1d2ac2118f5bffc767b
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-02-06 16:43:15 +0000

    Add function to create interleaved VDS

--- src/fastcs_eiger/controllers/odin/eiger_fp_adapter_controller.py
+++ src/fastcs_eiger/controllers/odin/eiger_fp_adapter_controller.py
@@ -1,4 +1,4 @@
-from fastcs.attributes import AttrRW
+from fastcs.attributes import AttrR, AttrRW
 from fastcs_odin.controllers.odin_data.frame_processor import (
     FrameProcessorAdapterController,
 )
@@ -7,3 +7,5 @@ from fastcs_odin.controllers.odin_data.frame_processor import (
 class EigerFrameProcessorAdapterController(FrameProcessorAdapterController):
     data_compression: AttrRW[str]
     data_datatype: AttrRW[str]
+    data_dims_0: AttrR[int]  # y
+    data_dims_1: AttrR[int]  # x
