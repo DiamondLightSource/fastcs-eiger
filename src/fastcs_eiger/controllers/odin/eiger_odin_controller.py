b8054c5fb441bcc549bd3cbe4fe5e22b1f8b83de
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-02-10 13:37:04 +0000

    Add attributes

--- src/fastcs_eiger/controllers/odin/eiger_odin_controller.py
+++ src/fastcs_eiger/controllers/odin/eiger_odin_controller.py
@@ -1,14 +1,19 @@
 import asyncio
 
+from fastcs.attributes import AttrRW
 from fastcs.connections import IPConnectionSettings
+from fastcs.datatypes import Bool
 from fastcs.methods import command
 
 from fastcs_eiger.controllers.eiger_controller import EigerController
+from fastcs_eiger.controllers.odin.generate_vds import create_interleave_vds
 from fastcs_eiger.controllers.odin.odin_controller import OdinController
 
 
 class EigerOdinController(EigerController):
     """Eiger controller with Odin sub controller"""
+
+    enable_vds_creation = AttrRW(Bool())
 
     def __init__(
         self,
@@ -58,3 +63,16 @@ class EigerOdinController(EigerController):
             await self.OD.writing.wait_for_value(True, timeout=3)
         except TimeoutError as e:
             raise TimeoutError("File writers failed to start") from e
+
+        if self.enable_vds_creation.get():
+            create_interleave_vds(
+                path=self.OD.file_path.get(),
+                frame_count=self.detector.nimages.get(),
+                frames_per_block=self.OD.block_size.get(),
+                blocks_per_file=self.OD.FP.process_blocks_per_file.get(),
+                frame_shape=(
+                    self.detector.x_pixels_in_detector.get(),
+                    self.detector.y_pixels_in_detector.get(),
+                ),
+                dtype=self.OD.FP.data_datatype.get(),
+            )
