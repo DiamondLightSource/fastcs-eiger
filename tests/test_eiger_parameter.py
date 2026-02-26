26981e2292df00c80da71c494cafb3d803a23b0b
Gary Yendell <gary.yendell@diamond.ac.uk>
2026-02-26 09:29:57 +0000

    Parameterize test

--- tests/test_eiger_parameter.py
+++ tests/test_eiger_parameter.py
@@ -1,46 +1,27 @@
+import pytest
+
 from fastcs_eiger.eiger_parameter import EigerParameterRef, EigerParameterResponse
 
 
-def test_eiger_access_mode():
-
+@pytest.mark.parametrize(
+    "api_version, mode, response_access_mode, expected_access_mode",
+    [
+        ("1.8.0", "config", "r", "r"),  # If access_mode exists, mode ignored
+        ("1.8.0", "status", "rw", "rw"),
+        ("1.6.0", "status", None, "r"),  # If no access_mode, infer from mode
+        ("1.6.0", "config", None, "rw"),
+    ],
+)
+def test_eiger_access_mode(
+    api_version, mode, response_access_mode, expected_access_mode
+):
     ref = EigerParameterRef(
         key="dummy_uri",
         subsystem="detector",
-        api_version="1.8.0",
-        mode="config",
-        response=EigerParameterResponse(access_mode="r", value=0.0, value_type="float"),
-    )
-    assert ref.access_mode == "r"
-
-    ref = EigerParameterRef(
-        key="dummy_uri",
-        subsystem="detector",
-        api_version="1.8.0",
-        mode="status",
+        api_version=api_version,
+        mode=mode,
         response=EigerParameterResponse(
-            access_mode="rw", value=0.0, value_type="float"
+            access_mode=response_access_mode, value=0.0, value_type="float"
         ),
     )
-    assert ref.access_mode == "rw"
-
-    ref = EigerParameterRef(
-        key="dummy_uri",
-        subsystem="detector",
-        api_version="1.6.0",
-        mode="status",
-        response=EigerParameterResponse(
-            access_mode=None, value=0.0, value_type="float"
-        ),
-    )
-    assert ref.access_mode == "r"
-
-    ref = EigerParameterRef(
-        key="dummy_uri",
-        subsystem="detector",
-        api_version="1.6.0",
-        mode="config",
-        response=EigerParameterResponse(
-            access_mode=None, value=0.0, value_type="float"
-        ),
-    )
-    assert ref.access_mode == "rw"
+    assert ref.access_mode == expected_access_mode
